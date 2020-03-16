1ca62095149e5630a4caefd5b9802c4167dfbf8c
Dev <dev234tripathi@gmail.com>
2020-03-16 17:09:42 +0530

    Add files via upload

---
+++ Heap/topKFrequentElements.java
@@ -0,0 +1,69 @@
+/**
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+*/
+
+// use maxHeap. Put entry into maxHeap so we can always poll a number with largest frequency
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+           
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = 
+                         new PriorityQueue<>((a,b)->(b.getValue()-a.getValue()));
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            maxHeap.add(entry);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        while(res.size()<k){
+            Map.Entry<Integer, Integer> entry = maxHeap.poll();
+            res.add(entry.getKey());
+        }
+        return res;
+    }
+}
+
+// use an array to save numbers into different bucket whose index is the frequency
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+        
+        // corner case: if there is only one number in nums, we need the bucket has index 1.
+        List<Integer>[] bucket = new List[nums.length+1];
+        for(int n:map.keySet()){
+            int freq = map.get(n);
+            if(bucket[freq]==null)
+                bucket[freq] = new LinkedList<>();
+            bucket[freq].add(n);
+        }
+        
+        List<Integer> res = new LinkedList<>();
+        for(int i=bucket.length-1; i>0 && k>0; --i){
+            if(bucket[i]!=null){
+                List<Integer> list = bucket[i]; 
+                res.addAll(list);
+                k-= list.size();
+            }
+        }
+        
+        return res;
+    }
+}
